3f083e9457dec187fde0c237a0a967ae71371dfa
Muhammad Haris <haris5544pk.com>
2025-04-08 10:05:19 -0700

    First Commit HEHE

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hello I'm Muhammad Haris </span> a Software Engineer who enjoys building websites and applications using the MERN stack. I specialize in both front-end and back-end development, creating websites that are easy to use and perform well. My goal is to build great user experiences and ensure the technology behind them works smoothly, I am a good communicator and enjoy solving problems. I always look for new challenges and ways to improve my skills Feel free to connect with me! You can reach out via LinkedIn, email, or GitHub if you'd like to talk about projects or collaborations."
+      ],
+      typeSpeed: 8,
+      contentType: 'html'
+    });
+  }, 100);
